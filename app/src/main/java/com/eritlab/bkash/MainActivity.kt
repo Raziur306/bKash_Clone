de6c82593e9fafeec9000c59f0ccbeaced38b098
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-01-09 20:46:38 +0600

    initial commit

---
+++ app/src/main/java/com/eritlab/bkash/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.eritlab.bkash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eritlab.bkash.ui.theme.BKashTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BKashTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
